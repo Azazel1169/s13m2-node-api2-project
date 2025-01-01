352f1016d4b7bc8338d04bb546bfb58d8dd7c552
Josh Rime <joshrime1990@icloud.com>
2024-12-31 22:45:01 -0600

    get all and get id work and pass test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,4 +3,20 @@ const express = require('express')
 
 const router = express.Router()
 
+router.get('/', (req,res) => {
+    res.status(200).json({
+        message: 'hello'
+    })
+})
+
+router.get('/:id', (req,res) => {})
+
+router.get('/:id/comments', (req,res) => {})
+
+router.post('/', (req,res) => {})
+
+router.delete('/:id', (req,res) => {})
+
+router.put('/id', (req,res) => {})
+
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 // require your posts router and connect it here
 const express = require("express");
 const postRouter = require("./posts/posts-router");
+
 const server = express();
 
 server.use(express.json());
